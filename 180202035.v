f1056a073f707ea7de5f0b912479b8d02b4b7d79
180202035 <34303354+180202035@users.noreply.github.com>
2020-11-06 16:16:22 +0300

    Update 180202035.v

--- 180202035.v
+++ 180202035.v
@@ -6,9 +6,9 @@ output [3:0]Sum;
 output  Cout;
 wire C1,C2,C3;
 
-FullAdder_m FA1(Sum[0],C1,X[0],Y[0],Cin);
-FullAdder_m FA2(Sum[1],C2,X[1],Y[1],C1);
-FullAdder_m FA3(.S(Sum[2]),.Cout(C3),.A(X[2]),.B(Y[2]),.Cin(C2));
-FullAdder_m FA4(.S(Sum[3]),.Cout(Cout),.A(X[3]),.B(Y[3]),.Cin(C3));
+FullAdder_m Z1(Sum[0],C1,X[0],Y[0],Cin);
+FullAdder_m Z2(Sum[1],C2,X[1],Y[1],C1);
+FullAdder_m Z3(.S(Sum[2]),.Cout(C3),.A(X[2]),.B(Y[2]),.Cin(C2));
+FullAdder_m Z4(.S(Sum[3]),.Cout(Cout),.A(X[3]),.B(Y[3]),.Cin(C3));
 
 endmodule
