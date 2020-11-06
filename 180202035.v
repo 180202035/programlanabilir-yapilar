25770a35645085c6d00d3029f19c1ed73f417734
180202035 <34303354+180202035@users.noreply.github.com>
2020-11-06 15:59:32 +0300

    Ödev_1

---
+++ 180202035.v
@@ -0,0 +1,14 @@
+module FourBit_fulladder(Sum ,Cout , Cin , X ,Y);
+
+input  Cin;
+input  [3:0]X,Y;
+output [3:0]Sum;
+output  Cout;
+wire C1,C2,C3;
+
+FullAdder_m FA1(Sum[0],C1,X[0],Y[0],Cin);
+FullAdder_m FA2(Sum[1],C2,X[1],Y[1],C1);
+FullAdder_m FA3(.S(Sum[2]),.Cout(C3),.A(X[2]),.B(Y[2]),.Cin(C2));
+FullAdder_m FA4(.S(Sum[3]),.Cout(Cout),.A(X[3]),.B(Y[3]),.Cin(C3));
+
+endmodule
