3a6f08c03f65e68c2e9d6734131caa2939cd2da4
180202035 <34303354+180202035@users.noreply.github.com>
2020-11-06 16:34:56 +0300

    Update 180202035.v

--- 180202035.v
+++ 180202035.v
@@ -1,3 +1,9 @@
+X = input ("Girdi giriniz")
+Y = input ("Girdi giriniz")
+Cin = input ("Girdi giriniz")
+
+
+
 module FourBit_fulladder(Sum ,Cout , Cin , X ,Y);
 
 input  Cin;
@@ -6,9 +12,20 @@ output [3:0]Sum;
 output  Cout;
 wire C1,C2,C3;
 
-FullAdder_m Z1(Sum[0],C1,X[0],Y[0],Cin);
-FullAdder_m Z2(Sum[1],C2,X[1],Y[1],C1);
-FullAdder_m Z3(.S(Sum[2]),.Cout(C3),.A(X[2]),.B(Y[2]),.Cin(C2));
-FullAdder_m Z4(.S(Sum[3]),.Cout(Cout),.A(X[3]),.B(Y[3]),.Cin(C3));
+AdderModul Z1(Sum[0],C1,X[0],Y[0],Cin);
+AdderModul Z2(Sum[1],C2,X[1],Y[1],C1);
+AdderModul Z3(.S(Sum[2]),.Cout(C3),.A(X[2]),.B(Y[2]),.Cin(C2));
+AdderModul Z4(.S(Sum[3]),.Cout(Cout),.A(X[3]),.B(Y[3]),.Cin(C3));
+
+endmodule
+
+module AdderModul( input A,
+       input B,
+       input CIN,
+       output S,
+       output COUT
+       );
+       assign S = A^B^CIN;
+       assign COUT = (A&B) | (CIN&(A^B));
 
 endmodule
